5b3cfed481eb5ac4d43b546f2fea1ee04bcbc1c0
Alex <alex.zhelyazko@yandex.by>
2020-03-29 22:54:18 +0300

    add: Singolo: Responsive

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 'use strict'
 
+document.addEventListener('scroll',onScroll);
+function onScroll(event) {
+    const curPos = pageYOffset;
+    console.log(curPos);
+    const links = document.querySelectorAll('#section-list a');
+    const sections = document.querySelectorAll('body>section');
+
+    sections.forEach((el)=>{
+        if((el.offsetTop-89) <= curPos && (el.offsetTop-89 + el.offsetHeight) > curPos){
+            links.forEach((a)=>{
+                a.classList.remove('active-li');
+                if(el.getAttribute('id') === a.getAttribute('href').substring(1)){
+                    a.classList.add('active-li');
+                }
+            })
+        }
+    });
+}
+
 const anchors = document.querySelectorAll('a[href*="#"]')
 
 for (let anchor of anchors) {
@@ -15,59 +34,84 @@ for (let anchor of anchors) {
   })
 }
 
-let phones = document.getElementsByClassName('Phone1')[0];
-phones.addEventListener("click", ()=>{
-  let blackphone = document.getElementsByClassName("black_phone")[0];
-  let op0 = "opacity: 0";
-  let op1 = "opacity: 1";
-  blackphone.style.opacity == 0 ? blackphone.style.opacity = 1: blackphone.style.opacity = 0;
-})
-
-let phones2 = document.getElementsByClassName('Phone1')[1];
-phones2.addEventListener("click", ()=>{
-  let blackphone = document.getElementsByClassName("black_phone")[0];
-  let op0 = "opacity: 0";
-  let op1 = "opacity: 1";
-  blackphone.style.opacity == 0 ? blackphone.style.opacity = 1: blackphone.style.opacity = 0;
-})
-
-let phones3 = document.getElementsByClassName('Phone2')[0];
-phones3.addEventListener("click", ()=>{
-  let blackphone = document.getElementsByClassName("black_phone2")[0];
-  let op0 = "opacity: 0";
-  let op1 = "opacity: 1";
-  blackphone.style.opacity == 0 ? blackphone.style.opacity = 1: blackphone.style.opacity = 0;
-})
-
-let phones4 = document.getElementsByClassName('Phone2')[1];
-phones4.addEventListener("click", ()=>{
-  let blackphone = document.getElementsByClassName("black_phone2")[0];
-  let op0 = "opacity: 0";
-  let op1 = "opacity: 1";
-  blackphone.style.opacity == 0 ? blackphone.style.opacity = 1: blackphone.style.opacity = 0;
-})
-
-let slide1 = document.getElementsByClassName('first_page')[0];
-let slide2 = document.getElementsByClassName("second_page")[0];
-let prev = document.getElementsByClassName('prev_chev')[0];
-
-
-const left_arr = document.getElementsByClassName("prev_chev")[0];
-const right_arr = document.getElementsByClassName("next_chev")[0];
-const pages = document.getElementsByClassName("page");
-left_arr.addEventListener("click", ()=>{
-  const active_slide = document.getElementsByClassName('active');
-  pages[0].parentNode.insertBefore(pages[3], pages[0]);
-  active_slide[0].classList.remove("active");
-  pages[1].classList.add("active");
-})
-
-right_arr.addEventListener("click", ()=>{
-  const active_slide = document.getElementsByClassName('active');
-  pages[3].parentNode.insertBefore(pages[0], null);
-  active_slide[0].classList.remove("active");
-  pages[1].classList.add("active");
-})
+const SlidersIdList = ['SliderBlock', 'slider-image'];
+let indexOfActiveSlide = 0;
+let isEnabled = true;
+document.getElementById('leftChev').addEventListener('click', (e) => {
+    if (isEnabled) {
+        isEnabled = false;
+        let width = document.getElementById('SliderBlock').offsetWidth;
+        if (indexOfActiveSlide == 0) {
+            document.getElementById('slider-image').style.left = `-100%`;
+            indexOfActiveSlide = 1;
+        } else {
+            document.getElementById('SliderBlock').style.left = `-50%`;
+            indexOfActiveSlide = 0;
+        }
+        const slider = setInterval(() => {
+            if (indexOfActiveSlide == 1) {
+                document.getElementById('SliderBlock').style.left = +document.getElementById('SliderBlock').style.left.replace('%', '') + 1 + '%';
+                document.getElementById('slider-image').style.left = +document.getElementById('slider-image').style.left.replace('%', '') + 1 + '%';
+                if (document.getElementById(`${SlidersIdList[0]}`).style.left == `50%`) {
+                    isEnabled = true;
+                    clearInterval(slider);
+                }
+            } else {
+                document.getElementById('slider-image').style.left = +document.getElementById('slider-image').style.left.replace('%', '') + 1 + '%';
+                document.getElementById('SliderBlock').style.left = +document.getElementById('SliderBlock').style.left.replace('%', '') + 1 + '%';
+                if (document.getElementById('SliderBlock').style.left == '0%') {
+                    isEnabled = true;
+                    clearInterval(slider);
+                }
+            }
+        }, 10);
+    }  
+});
+document.getElementById('rightChev').addEventListener('click', (e) => {
+    if (isEnabled) {
+        isEnabled = false;
+        let width = document.getElementById('SliderBlock').offsetWidth;
+        if (indexOfActiveSlide == 0) {
+            document.getElementById('slider-image').style.left = "0%";
+            indexOfActiveSlide = 1;
+        } else {
+            document.getElementById('SliderBlock').style.left = `50%`;
+            indexOfActiveSlide = 0;
+        }
+        const slider = setInterval(() => {
+            if (indexOfActiveSlide == 1) {
+                document.getElementById('SliderBlock').style.left = +document.getElementById('SliderBlock').style.left.replace('%', '') - 1 + '%';
+                document.getElementById('slider-image').style.left = +document.getElementById('slider-image').style.left.replace('%', '') - 1 + '%';
+                if (document.getElementById(`${SlidersIdList[0]}`).style.left == `-50%`) {
+                    isEnabled = true;
+                    clearInterval(slider);
+                }
+            } else {
+                document.getElementById('slider-image').style.left = +document.getElementById('slider-image').style.left.replace('%', '') - 1 + '%';
+                document.getElementById('SliderBlock').style.left = +document.getElementById('SliderBlock').style.left.replace('%', '') - 1 + '%';
+                if (document.getElementById('SliderBlock').style.left == '0%') {
+                    isEnabled = true;
+                    clearInterval(slider);
+                }
+            }
+        }, 10);
+    }
+});
+// Buttons of Phones
+document.getElementById('ButtonOfHorizontalIPhone').addEventListener('click', () => {
+    if (document.getElementById('HorizontalIphone').getAttribute('src') == 'assets/HorizontalIphone.png') {
+        document.getElementById('HorizontalIphone').setAttribute('src', 'assets/HorizontalIphoneOff.png');
+    } else {
+        document.getElementById('HorizontalIphone').setAttribute('src', 'assets/HorizontalIphone.png');
+    }
+});
+document.getElementById('ButtonOfVerticalIPhone').addEventListener('click', () => {
+    if (document.getElementById('VerticalIphone').getAttribute('src') == 'assets/VerticalIphone.png') {
+        document.getElementById('VerticalIphone').setAttribute('src', 'assets/VerticalIphoneOff.png');
+    } else {
+        document.getElementById('VerticalIphone').setAttribute('src', 'assets/VerticalIphone.png');
+    }
+});
 
 const Tab =  document.getElementById('tabs');
 Tab.addEventListener('click', (event)=>{
@@ -104,6 +148,7 @@ BUTTON.addEventListener('click', () => {
     alert("Провертье введенные данные и email");
     return 0;
   }
+  
   if (subject != ''){
     subject = "Тема: " + subject;
   }
@@ -120,3 +165,22 @@ CLOSE_BUTTON.addEventListener('click', () => {
   document.getElementById('Form').reset();
   hidden_part.style.display = 'none';
 });
+
+const Burger = document.getElementById('burger');
+Burger.addEventListener('click',(event)=>{
+    if(Burger.classList.contains('activeham')){
+        Burger.classList.remove('activeham');
+        document.getElementById('navigation').style.display="none";
+        document.getElementById('logo').classList.remove('logo-for-burger');
+        document.getElementById('pop-window').style.display="none";
+        document.getElementById('message').style.display="block";
+    }
+    else{
+        Burger.classList.add('activeham');
+        document.getElementById('navigation').style.display="block";
+        document.getElementById('logo').classList.add('logo-for-burger');
+        document.getElementById('pop-window').style.display="block";
+        document.getElementById('message').style.display="none";
+    }
+
+})
